6a8fcee3d12fbeef4bfad45527dd9eac867adb04
Niya Mateeva Yondzhju <119063181+Polishko@users.noreply.github.com>
2025-04-04 21:42:47 +0300

    Update slider.js
    Update: Updated js-legacy files to reflect changes in ts files

--- src/js-legacy/components/slider.js
+++ src/js-legacy/components/slider.js
@@ -1,38 +1,49 @@
-let slideIndex = 1;
-showSlides(slideIndex);
-
-export function plusSlides(n) {
-  slideIndex += n;
+export function initSlider() {
+  let slideIndex = 1;
   showSlides(slideIndex);
-}
 
-export function currentSlide(n) {
-  slideIndex = n;
-  showSlides(slideIndex);
-}
+  function showSlides(n) {
+    const slides = document.getElementsByClassName(
+      "slides"
+    );
+    const dots = document.getElementsByClassName(
+      "demo"
+    );
+    const captionText = document.getElementById(
+      "caption"
+    )
 
-function showSlides(n) {
-  let slides = document.getElementsByClassName("slides");
-  let dots = document.getElementsByClassName("demo");
-  let captionText = document.getElementById("caption");
-  
-  if (n > slides.length) {
-    slideIndex = 1;
+    if (slides) {
+      if (n > slides.length) {
+        slideIndex = 1;
+      }
+
+      if (n < 1) {
+        slideIndex = slides.length;
+      }
+
+      for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+      }
+
+      if (dots) {
+        for (let i = 0; i < dots.length; i++) {
+          dots[i].className = dots[i].className.replace(" active", "");
+        }
+
+        slides[slideIndex - 1].style.display = "block";
+        dots[slideIndex - 1].className += " active";
+
+        if (captionText) {
+          captionText.innerHTML = dots[slideIndex - 1].alt;
+        }
+      }
+    }
   }
 
-  if (n < 1) {
-    slideIndex = slides.length;
-  }
-
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-
-  for (let i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-  captionText.innerHTML = dots[slideIndex - 1].alt;
+  return {
+    next: () => showSlides(++slideIndex),
+    prev: () => showSlides(--slideIndex),
+    goTo: (n) => showSlides((slideIndex = n)),
+  };
 }
